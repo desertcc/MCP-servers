fb06b27d0d80b07754b772d08b77245031b3a7fc
desertcc <desert0218@gmail.com>
2025-05-02 02:02:18 -0700

    cron

---
+++ servers/reddit_server/run_bot_cron.py
@@ -0,0 +1,17 @@
+import time
+import os
+import subprocess
+
+def run_bot():
+    print("Running bot script...")
+    subprocess.run([
+        "python", "servers/reddit_server/bot_runner.py",
+        "--max-subreddits", "10",
+        "--max-replies", "5",
+        "--max-upvotes", "10"
+    ])
+
+while True:
+    run_bot()
+    print("Sleeping for 8 hours...")
+    time.sleep(8 * 60 * 60)  # Run every 8 hours
